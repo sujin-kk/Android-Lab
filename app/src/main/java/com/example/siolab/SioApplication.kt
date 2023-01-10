75e0f5ac7a12238e53d58aebf22be387450b4b08
sujin-kk <sjoo0002@gmail.com>
2023-01-10 19:27:18 +0900

    chore: add timber

--- app/src/main/java/com/example/siolab/SioApplication.kt
+++ app/src/main/java/com/example/siolab/SioApplication.kt
@@ -1,9 +1,12 @@
 package com.example.siolab
 
 import android.app.Application
+import timber.log.Timber
 
 class SioApplication: Application() {
     override fun onCreate() {
         super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
     }
 }
