7da04854889c621c558cf0de33508c2f42d7be59
sujin-kk <sjoo0002@gmail.com>
2023-01-13 18:43:05 +0900

    feat: 송금 카드 테두리 추가한다.

--- app/src/main/java/com/example/siolab/presentation/ui/send/SendBottomSheetDialog.kt
+++ app/src/main/java/com/example/siolab/presentation/ui/send/SendBottomSheetDialog.kt
@@ -13,7 +13,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,11 +26,14 @@ fun SendBottomSheet(onNext: () -> Unit) {
         Text(
             text = stringResource(id = R.string.send_money_bottom_label),
             fontWeight = FontWeight.Bold,
-            fontSize = 20.sp
+            fontSize = 20.sp,
+            modifier = Modifier.padding(bottom = 24.dp)
         )
 
         WayToSendCheckCard(isCheck = true, isNow = false)
         WayToSendCheckCard(isCheck = false, isNow = true)
+
+        Spacer(modifier = Modifier.size(20.dp))
 
         SentbeFooterButton(onClick = onNext, buttonText = "다음")
     }
@@ -51,9 +53,15 @@ fun WayToSendCheckCard(isCheck: Boolean, isNow: Boolean) {
             else R.string.send_money_bottom_later_desc
         )
 
-    Surface() {
-        Row(verticalAlignment = Alignment.CenterVertically) {
+    Surface(
+        border = BorderStroke(0.5.dp, colorResource(R.color.gray_200)),
+        shape = RoundedCornerShape(6.dp),
+        modifier = Modifier.padding(bottom = 20.dp)
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(16.dp)) {
             if (isCheck) CheckedBox() else UnCheckedBox()
+
+            Spacer(Modifier.size(8.dp))
 
             Column() {
                 Text(text = wayToSendLabel, fontSize = 16.sp, fontWeight = FontWeight.Bold)
