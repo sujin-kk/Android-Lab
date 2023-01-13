6e2052e10ada26b2fbcbabe8eb46dcf3a876f4b6
sujin-kk <sjoo0002@gmail.com>
2023-01-13 18:36:41 +0900

    feat: 카드 체크 상태에 따라 색깔 바뀌게 한다.

--- app/src/main/java/com/example/siolab/presentation/ui/send/SendBottomSheetDialog.kt
+++ app/src/main/java/com/example/siolab/presentation/ui/send/SendBottomSheetDialog.kt
@@ -1,13 +1,16 @@
 package com.example.siolab.presentation.ui.send
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
@@ -27,15 +30,15 @@ fun SendBottomSheet(onNext: () -> Unit) {
             fontSize = 20.sp
         )
 
-        WayToSendCheckBox(isCheck = true, isNow = false)
-        WayToSendCheckBox(isCheck = false, isNow = true)
+        WayToSendCheckCard(isCheck = true, isNow = false)
+        WayToSendCheckCard(isCheck = false, isNow = true)
 
         SentbeFooterButton(onClick = onNext, buttonText = "다음")
     }
 }
 
 @Composable
-fun WayToSendCheckBox(isCheck: Boolean, isNow: Boolean) {
+fun WayToSendCheckCard(isCheck: Boolean, isNow: Boolean) {
     val wayToSendLabel =
         stringResource(
             id = if (isNow) R.string.send_money_bottom_now_label
@@ -49,15 +52,47 @@ fun WayToSendCheckBox(isCheck: Boolean, isNow: Boolean) {
         )
 
     Surface() {
-        Row() {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            if (isCheck) CheckedBox() else UnCheckedBox()
+
             Column() {
                 Text(text = wayToSendLabel, fontSize = 16.sp, fontWeight = FontWeight.Bold)
-                Text(text = wayToSendDesc)
+                Text(text = wayToSendDesc, modifier = Modifier.wrapContentSize())
             }
         }
     }
 }
 
+@Composable
+private fun CheckedBox() {
+    Surface(
+        border = BorderStroke(0.5.dp, colorResource(id = R.color.main_blue)),
+        shape = CircleShape,
+        modifier = Modifier.size(size = 20.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(4.dp)
+                .clip(CircleShape)
+                .background(color = colorResource(id = R.color.main_blue))
+        )
+    }
+}
+
+@Composable
+private fun UnCheckedBox() {
+    Surface(
+        border = BorderStroke(0.5.dp, colorResource(id = R.color.gray_200)),
+        shape = CircleShape,
+        modifier = Modifier.size(size = 20.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .clip(CircleShape)
+                .background(color = colorResource(id = R.color.white))
+        )
+    }
+}
 
 @Preview
 @Composable
